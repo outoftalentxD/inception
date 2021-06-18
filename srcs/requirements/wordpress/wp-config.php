bacc497f1029e67b1ba2a13277eb0ac9ead34f7b
melaena <melaena@student.21-school.ru>
2021-06-18 18:45:34 +0300

    evaulated version

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'melaena' );
 define( 'DB_PASSWORD', 'melaena' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'db' );
+define( 'DB_HOST', 'mariadb' );
 	
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
